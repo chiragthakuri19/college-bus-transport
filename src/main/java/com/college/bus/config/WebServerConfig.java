341ea071af73c62f25947eaa7399137a8f2b9499
chiragthakuri19 <chiragthakuri19@gmail.com>
2025-05-17 12:53:30 +0530

    Fix: Web server initialization and configuration

---
+++ src/main/java/com/college/bus/config/WebServerConfig.java
@@ -0,0 +1,40 @@
+package com.college.bus.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+
+@Configuration
+public class WebServerConfig {
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addConnectorCustomizers((connector) -> {
+            connector.setProperty("relaxedQueryChars", "[]{}|");
+            connector.setProperty("maxHttpHeaderSize", "65536");
+            connector.setProperty("maxThreads", "50");
+            connector.setProperty("acceptCount", "10");
+        });
+        return tomcat;
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                    .allowedOrigins("*")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    .allowedHeaders("*")
+                    .allowCredentials(true)
+                    .maxAge(3600);
+            }
+        };
+    }
+} 
