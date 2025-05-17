be9261a3c48264c0db17e761d4610e05f88f1d1e
chiragthakuri19 <chiragthakuri19@gmail.com>
2025-05-17 12:56:58 +0530

    Fix: JPA configuration and EntityManager initialization

--- src/main/java/com/college/bus/config/ApplicationConfig.java
+++ src/main/java/com/college/bus/config/ApplicationConfig.java
@@ -1,21 +1,29 @@
 package com.college.bus.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.Bean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 
+import jakarta.persistence.EntityManagerFactory;
 import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
+import javax.sql.DataSource;
 
 @Configuration
 @EnableConfigurationProperties
 @EntityScan(basePackages = "com.college.bus.model")
+@EnableJpaRepositories(basePackages = "com.college.bus.repository")
+@EnableTransactionManagement
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class ApplicationConfig {
 
@@ -30,5 +38,26 @@ public class ApplicationConfig {
                 servletContext.setInitParameter("spring.jpa.open-in-view", "false");
             }
         };
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource);
+        em.setPackagesToScan("com.college.bus.model");
+
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setGenerateDdl(true);
+        vendorAdapter.setShowSql(false);
+        em.setJpaVendorAdapter(vendorAdapter);
+
+        return em;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory);
+        return transactionManager;
     }
 } 
